e12e62c7dd7825c60324e7fa96c68e61a18c4105
WindWT <leibao0723@gmail.com>
2014-09-29 17:04:56 +0800

    remove Plist.net, iOS may not work well

--- RTDDataProvider/FileParser.cs
+++ RTDDataProvider/FileParser.cs
@@ -5,60 +5,16 @@ using System.Web;
 using System.Xml;
 using System.Data;
 using System.Reflection;
-using PList;
 using System.IO;
 using RTDDataProvider.MasterData;
+using MsgPack;
+using MsgPack.Serialization;
 
 namespace RTDDataProvider
 {
     public static class FileParser
     {
         #region Android
-        public static DataSet ParseXmlMDB(string xmlMDBString)
-        {
-            XmlDocument xmlMDB = new XmlDocument();
-            DataSet ds = new DataSet("MDB");
-
-            xmlMDB.LoadXml(xmlMDBString);
-            foreach (XmlNode xmlNode in xmlMDB.GetElementsByTagName("string"))
-            {
-                if (xmlNode.Attributes["name"] != null)
-                {
-                    string MDBID = xmlNode.Attributes["name"].Value;
-                    string MDBenumID = MDBID.Replace("MDBS", String.Empty);
-                    string jsonMDB = xmlNode.InnerText;
-                    DataTable dt = JSON.ParseJSONMDB(jsonMDB, (MASTERDB)Enum.Parse(typeof(MASTERDB), MDBenumID, true));
-                    ds.Tables.Add(dt);
-                }
-            }
-            return ds;
-        }
-        public static DataTable ParseXmlLDB(string xmlLDBString)
-        {
-            XmlDocument xmlLDB = new XmlDocument();
-            DataTable dt = new DataTable("LEVEL_DATA_MASTER");
-            DataColumn dc = null;
-            foreach (FieldInfo fi in typeof(LevelDataMaster).GetFields())
-            {
-                dc = new DataColumn();
-                dc.ColumnName = fi.Name;
-                dc.DataType = fi.FieldType;
-                dt.Columns.Add(dc);
-            }
-            xmlLDB.LoadXml(xmlLDBString);
-            foreach (XmlNode xmlNode in xmlLDB.GetElementsByTagName("string"))
-            {
-                if (xmlNode.Attributes["name"] != null && xmlNode.Attributes["name"].Value.StartsWith("LDBS"))
-                {
-                    string jsonLDB = xmlNode.InnerText;
-                    DataTable dtTemp = JSON.ParseJSONLDB(jsonLDB);
-                    object[] obj = new object[dtTemp.Columns.Count];
-                    dtTemp.Rows[0].ItemArray.CopyTo(obj, 0);
-                    dt.Rows.Add(obj);
-                }
-            }
-            return dt;
-        }
         public static DataSet ParseXmlGAME(string xmlGAMEString)
         {
             XmlDocument xmlGAME = new XmlDocument();
@@ -138,32 +94,31 @@ namespace RTDDataProvider
         #endregion
 
         #region iOS
-        public static DataSet ParsePlistMDB(Stream plistFileStream)
+
+
+        #region Android_notworking
+        public static DataSet ParseXmlMDB(string xmlMDBString)
         {
-            PListRoot plist = PListRoot.Load(plistFileStream);
-            plist.Format = PListFormat.Binary;
-            PListDict dict = (PListDict)plist.Root;
+            XmlDocument xmlMDB = new XmlDocument();
             DataSet ds = new DataSet("MDB");
 
-            foreach (KeyValuePair<string, IPListElement> item in dict)
+            xmlMDB.LoadXml(xmlMDBString);
+            foreach (XmlNode xmlNode in xmlMDB.GetElementsByTagName("string"))
             {
-                string key = item.Key;
-                if (!string.IsNullOrWhiteSpace(key) && key.StartsWith("MDBS", StringComparison.Ordinal))
+                if (xmlNode.Attributes["name"] != null)
                 {
-                    string MDBID = key;
+                    string MDBID = xmlNode.Attributes["name"].Value;
                     string MDBenumID = MDBID.Replace("MDBS", String.Empty);
-                    string jsonMDB = (PListString)item.Value;
+                    string jsonMDB = xmlNode.InnerText;
                     DataTable dt = JSON.ParseJSONMDB(jsonMDB, (MASTERDB)Enum.Parse(typeof(MASTERDB), MDBenumID, true));
                     ds.Tables.Add(dt);
                 }
             }
             return ds;
         }
-        public static DataTable ParsePlistLDB(Stream plistFileStream)
+        public static DataTable ParseXmlLDB(string xmlLDBString)
         {
-            PListRoot plist = PListRoot.Load(plistFileStream);
-            plist.Format = PListFormat.Binary;
-            PListDict dict = (PListDict)plist.Root;
+            XmlDocument xmlLDB = new XmlDocument();
             DataTable dt = new DataTable("LEVEL_DATA_MASTER");
             DataColumn dc = null;
             foreach (FieldInfo fi in typeof(LevelDataMaster).GetFields())
@@ -173,12 +128,12 @@ namespace RTDDataProvider
                 dc.DataType = fi.FieldType;
                 dt.Columns.Add(dc);
             }
-            foreach (KeyValuePair<string, IPListElement> item in dict)
+            xmlLDB.LoadXml(xmlLDBString);
+            foreach (XmlNode xmlNode in xmlLDB.GetElementsByTagName("string"))
             {
-                string key = item.Key;
-                if (!string.IsNullOrWhiteSpace(key) && key.StartsWith("LDBS", StringComparison.Ordinal))
+                if (xmlNode.Attributes["name"] != null && xmlNode.Attributes["name"].Value.StartsWith("LDBS"))
                 {
-                    string jsonLDB = (PListString)item.Value;
+                    string jsonLDB = xmlNode.InnerText;
                     DataTable dtTemp = JSON.ParseJSONLDB(jsonLDB);
                     object[] obj = new object[dtTemp.Columns.Count];
                     dtTemp.Rows[0].ItemArray.CopyTo(obj, 0);
@@ -187,50 +142,102 @@ namespace RTDDataProvider
             }
             return dt;
         }
-        public static DataTable ParsePlistFileMDB(Stream plistFileStream)
-        {
-            var reader = new System.Runtime.Serialization.Plists.BinaryPlistReader();
-            var dict = reader.ReadObject(plistFileStream);
-            string MDBenumID = (dict["$objects"] as Object[])[2].ToString().Replace("MDBS", String.Empty); ;
-            string jsonMDB = (dict["$objects"] as Object[])[3].ToString();
-            DataTable dt = JSON.ParseJSONMDB(jsonMDB, (MASTERDB)Enum.Parse(typeof(MASTERDB), MDBenumID, true));
-            return dt;
-        }
-        public static DataTable ParsePlistFileLDB(Stream plistFileStream)
-        {
-            DataTable dt = new DataTable("LEVEL_DATA_MASTER");
-            DataColumn dc = null;
-            foreach (FieldInfo fi in typeof(LevelDataMaster).GetFields())
-            {
-                dc = new DataColumn();
-                dc.ColumnName = fi.Name;
-                dc.DataType = fi.FieldType;
-                dt.Columns.Add(dc);
-            }
+        #endregion
 
-            var reader = new System.Runtime.Serialization.Plists.BinaryPlistReader();
-            var dict = reader.ReadObject(plistFileStream);
+        #region iOS_notworking
+        //public static DataSet ParsePlistMDB(Stream plistFileStream)
+        //{
+        //    PListRoot plist = PListRoot.Load(plistFileStream);
+        //    plist.Format = PListFormat.Binary;
+        //    PListDict dict = (PListDict)plist.Root;
+        //    DataSet ds = new DataSet("MDB");
 
-            Dictionary<int, int> objIndex = new Dictionary<int, int>();
-            object[] NSKeyObjects = (((dict["$objects"] as Object[])[1] as IDictionary<object, object>)["NS.keys"] as object[]);
-            object[] NSValueObjects = (((dict["$objects"] as Object[])[1] as IDictionary<object, object>)["NS.objects"] as object[]);
-            for (int i = 0; i < NSKeyObjects.Length; i++)
-            {
-                objIndex.Add(Convert.ToInt32((NSKeyObjects[i] as Dictionary<String, UInt64>)["CF$UID"]), Convert.ToInt32((NSValueObjects[i] as Dictionary<String, UInt64>)["CF$UID"]));
-            }
-            foreach (KeyValuePair<int, int> o in objIndex)
-            {
-                if ((dict["$objects"] as Object[])[o.Key].ToString().StartsWith("LDBS"))
-                {
-                    string jsonLDB = (dict["$objects"] as Object[])[o.Value].ToString();
-                    DataTable dtTemp = JSON.ParseJSONLDB(jsonLDB);
-                    object[] obj = new object[dtTemp.Columns.Count];
-                    dtTemp.Rows[0].ItemArray.CopyTo(obj, 0);
-                    dt.Rows.Add(obj);
-                }
-            }
-            return dt;
-        }
+        //    foreach (KeyValuePair<string, IPListElement> item in dict)
+        //    {
+        //        string key = item.Key;
+        //        if (!string.IsNullOrWhiteSpace(key) && key.StartsWith("MDBS", StringComparison.Ordinal))
+        //        {
+        //            string MDBID = key;
+        //            string MDBenumID = MDBID.Replace("MDBS", String.Empty);
+        //            string jsonMDB = (PListString)item.Value;
+        //            DataTable dt = JSON.ParseJSONMDB(jsonMDB, (MASTERDB)Enum.Parse(typeof(MASTERDB), MDBenumID, true));
+        //            ds.Tables.Add(dt);
+        //        }
+        //    }
+        //    return ds;
+        //}
+        //public static DataTable ParsePlistLDB(Stream plistFileStream)
+        //{
+        //    PListRoot plist = PListRoot.Load(plistFileStream);
+        //    plist.Format = PListFormat.Binary;
+        //    PListDict dict = (PListDict)plist.Root;
+        //    DataTable dt = new DataTable("LEVEL_DATA_MASTER");
+        //    DataColumn dc = null;
+        //    foreach (FieldInfo fi in typeof(LevelDataMaster).GetFields())
+        //    {
+        //        dc = new DataColumn();
+        //        dc.ColumnName = fi.Name;
+        //        dc.DataType = fi.FieldType;
+        //        dt.Columns.Add(dc);
+        //    }
+        //    foreach (KeyValuePair<string, IPListElement> item in dict)
+        //    {
+        //        string key = item.Key;
+        //        if (!string.IsNullOrWhiteSpace(key) && key.StartsWith("LDBS", StringComparison.Ordinal))
+        //        {
+        //            string jsonLDB = (PListString)item.Value;
+        //            DataTable dtTemp = JSON.ParseJSONLDB(jsonLDB);
+        //            object[] obj = new object[dtTemp.Columns.Count];
+        //            dtTemp.Rows[0].ItemArray.CopyTo(obj, 0);
+        //            dt.Rows.Add(obj);
+        //        }
+        //    }
+        //    return dt;
+        //}
+        //public static DataTable ParsePlistFileMDB(Stream plistFileStream)
+        //{
+        //    var reader = new System.Runtime.Serialization.Plists.BinaryPlistReader();
+        //    var dict = reader.ReadObject(plistFileStream);
+        //    string MDBenumID = (dict["$objects"] as Object[])[2].ToString().Replace("MDBS", String.Empty); ;
+        //    string jsonMDB = (dict["$objects"] as Object[])[3].ToString();
+        //    DataTable dt = JSON.ParseJSONMDB(jsonMDB, (MASTERDB)Enum.Parse(typeof(MASTERDB), MDBenumID, true));
+        //    return dt;
+        //}
+        //public static DataTable ParsePlistFileLDB(Stream plistFileStream)
+        //{
+        //    DataTable dt = new DataTable("LEVEL_DATA_MASTER");
+        //    DataColumn dc = null;
+        //    foreach (FieldInfo fi in typeof(LevelDataMaster).GetFields())
+        //    {
+        //        dc = new DataColumn();
+        //        dc.ColumnName = fi.Name;
+        //        dc.DataType = fi.FieldType;
+        //        dt.Columns.Add(dc);
+        //    }
+
+        //    var reader = new System.Runtime.Serialization.Plists.BinaryPlistReader();
+        //    var dict = reader.ReadObject(plistFileStream);
+
+        //    Dictionary<int, int> objIndex = new Dictionary<int, int>();
+        //    object[] NSKeyObjects = (((dict["$objects"] as Object[])[1] as IDictionary<object, object>)["NS.keys"] as object[]);
+        //    object[] NSValueObjects = (((dict["$objects"] as Object[])[1] as IDictionary<object, object>)["NS.objects"] as object[]);
+        //    for (int i = 0; i < NSKeyObjects.Length; i++)
+        //    {
+        //        objIndex.Add(Convert.ToInt32((NSKeyObjects[i] as Dictionary<String, UInt64>)["CF$UID"]), Convert.ToInt32((NSValueObjects[i] as Dictionary<String, UInt64>)["CF$UID"]));
+        //    }
+        //    foreach (KeyValuePair<int, int> o in objIndex)
+        //    {
+        //        if ((dict["$objects"] as Object[])[o.Key].ToString().StartsWith("LDBS"))
+        //        {
+        //            string jsonLDB = (dict["$objects"] as Object[])[o.Value].ToString();
+        //            DataTable dtTemp = JSON.ParseJSONLDB(jsonLDB);
+        //            object[] obj = new object[dtTemp.Columns.Count];
+        //            dtTemp.Rows[0].ItemArray.CopyTo(obj, 0);
+        //            dt.Rows.Add(obj);
+        //        }
+        //    }
+        //    return dt;
+        //}
         public static DataSet ParsePlistFileGAME(Stream plistFileStream)
         {
             DataSet ds = new DataSet("MDB");
@@ -283,29 +290,29 @@ namespace RTDDataProvider
             ds.Tables.Add(dtUTM);
             return ds;
         }
-        public static List<EnemyInfo> ParseEnemyInfo(string questId, Stream plistFileStream)
-        {
-            PListRoot plist = PListRoot.Load(plistFileStream);
-            plist.Format = PListFormat.Binary;
-            PListDict dict = (PListDict)plist.Root;
-            string jsonQuest = String.Empty, jsonEnemyInfo = String.Empty;
-            foreach (KeyValuePair<string, IPListElement> item in dict)
-            {
-                string key = item.Key;
-                if (!string.IsNullOrWhiteSpace(key))
-                {
-                    if (key == "RESTORE")   //this keyValue is not valid
-                    {
-                        jsonQuest = (PListString)item.Value;
-                    }
-                    else if (key == "QUEST_ENEMY_INFO")
-                    {
-                        jsonEnemyInfo = (PListString)item.Value;
-                    }
-                }
-            }
-            return JSON.ParseEnemyInfo(questId, jsonQuest, jsonEnemyInfo);
-        }
+        //public static List<EnemyInfo> ParseEnemyInfo(string questId, Stream plistFileStream)
+        //{
+        //    PListRoot plist = PListRoot.Load(plistFileStream);
+        //    plist.Format = PListFormat.Binary;
+        //    PListDict dict = (PListDict)plist.Root;
+        //    string jsonQuest = String.Empty, jsonEnemyInfo = String.Empty;
+        //    foreach (KeyValuePair<string, IPListElement> item in dict)
+        //    {
+        //        string key = item.Key;
+        //        if (!string.IsNullOrWhiteSpace(key))
+        //        {
+        //            if (key == "RESTORE")   //this keyValue is not valid
+        //            {
+        //                jsonQuest = (PListString)item.Value;
+        //            }
+        //            else if (key == "QUEST_ENEMY_INFO")
+        //            {
+        //                jsonEnemyInfo = (PListString)item.Value;
+        //            }
+        //        }
+        //    }
+        //    return JSON.ParseEnemyInfo(questId, jsonQuest, jsonEnemyInfo);
+        //}
         #endregion
     }
 }
