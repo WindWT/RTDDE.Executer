d9a1f7ad1841ed035fe652f4d4296dca834f394f
WindWT <leibao0723@gmail.com>
2015-01-06 22:27:43 +0800

    replace FreeImage with DDSReader

--- RTDDE.Executer/Func/Model.xaml.cs
+++ RTDDE.Executer/Func/Model.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
-using FreeImageAPI;
+using DDSReader;
 using HelixToolkit.Wpf;
 using RTDDE.Provider;
 
@@ -21,20 +21,6 @@ namespace RTDDE.Executer.Func
         public Model()
         {
             InitializeComponent();
-            if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(this) == false)
-            {
-                if (File.Exists("FreeImage.dll") == false)
-                {
-                    if (Environment.Is64BitProcess)
-                    {
-                        System.IO.File.Copy(@"x64\FreeImage.dll", "FreeImage.dll", true);
-                    }
-                    else
-                    {
-                        System.IO.File.Copy(@"x86\FreeImage.dll", "FreeImage.dll", true);
-                    }
-                }
-            }
         }
 
         public void Refresh()
@@ -68,20 +54,17 @@ namespace RTDDE.Executer.Func
         private ModelInfo modelInfo = new ModelInfo();
         public void Load(int g_id, string model)
         {
-            lock (modelInfo)
-            {
+            lock (modelInfo) {
                 LoadingMask.Visibility = Visibility.Visible;
                 modelInfo = new ModelInfo(g_id, model);
                 //List<string> fileList = InitModelFile(modelInfo);
                 Task<List<string>> taskInitModelFile = new Task<List<string>>(() => InitModelFile(modelInfo));
                 taskInitModelFile.ContinueWith(t =>
                     {
-                        try
-                        {
+                        try {
                             InitModel(t.Result);
                         }
-                        catch (Exception ex)
-                        {
+                        catch (Exception ex) {
                             File.Delete(modelInfo.GetUnity3DFilePath());
                             Directory.Delete(modelInfo.GetModelDirectory(), true);
                             Utility.ShowException(ex.Message);
@@ -97,37 +80,30 @@ namespace RTDDE.Executer.Func
         }
         private List<string> InitModelFile(ModelInfo info)
         {
-            if (AssetBundleDefine.m_BasicBundles.Any(o => o.fileName == modelInfo.GetFileName()) == false)
-            {
+            if (AssetBundleDefine.m_BasicBundles.Any(o => o.fileName == modelInfo.GetFileName()) == false) {
                 return new List<string>();
             }
             AssetBundleDefine.AssetBundleInfo abi = AssetBundleDefine.m_BasicBundles.First(o => o.fileName == modelInfo.GetFileName());
-            if (Directory.Exists("model") == false)
-            {
+            if (Directory.Exists("model") == false) {
                 Directory.CreateDirectory("model");
             }
             //first find exist model file
             List<string> fileList = GetUnpackFile(modelInfo);
-            if (fileList.Count > 0)
-            {
+            if (fileList.Count > 0) {
                 return fileList;
             }
-            else if (string.IsNullOrEmpty(Settings.Config.Model.DisunityPath))
-            {
+            else if (string.IsNullOrEmpty(Settings.Config.Model.DisunityPath)) {
                 //disable unpack & download if no disunity
                 return fileList;
             }
-            else if (File.Exists(info.GetUnity3DFilePath()))
-            {
+            else if (File.Exists(info.GetUnity3DFilePath())) {
                 //find unity3d file, then extract
                 UnpackFile(modelInfo);
                 return GetUnpackFile(modelInfo);
             }
-            else
-            {
+            else {
                 //download,extract,return
-                using (WebClient client = new WebClient())
-                {
+                using (WebClient client = new WebClient()) {
                     //client.DownloadFileCompleted += (sender, e) =>
                     //{
                     //    UnpackFile(modelInfo);
@@ -141,8 +117,7 @@ namespace RTDDE.Executer.Func
         }
         private void UnpackFile(ModelInfo info)
         {
-            using (Process p = new Process())
-            {
+            using (Process p = new Process()) {
                 p.StartInfo.UseShellExecute = false;
                 p.StartInfo.CreateNoWindow = true;
                 p.StartInfo.FileName = Settings.Config.Model.DisunityPath;
@@ -155,26 +130,22 @@ namespace RTDDE.Executer.Func
         {
             List<string> unpacked = new List<string>();
             DirectoryInfo di = new DirectoryInfo(info.GetModelDirectory());
-            if (di.Exists)
-            {
-                foreach (FileInfo fi in di.GetFiles("*.*", SearchOption.AllDirectories))
-                {
+            if (di.Exists) {
+                foreach (FileInfo fi in di.GetFiles("*.*", SearchOption.AllDirectories)) {
                     string unpackedFileName = System.IO.Path.GetFileNameWithoutExtension(fi.FullName);
-                    if (unpackedFileName.Contains(info.modelName) || info.modelName.Contains(unpackedFileName))
-                    {
+                    if (unpackedFileName.Contains(info.modelName) || info.modelName.Contains(unpackedFileName)) {
                         unpacked.Add(fi.FullName);
                     }
                 }
             }
             return unpacked;
         }
-        private void InitModel(List<string> fileList)
+        private async void InitModel(List<string> fileList)
         {
             string objFilePath = fileList.Find(o => o.EndsWith(".obj"));
             string ddsFilePath = fileList.Find(o => o.EndsWith(".dds") && (o.Contains("wpn.dss") == false));
             string wpnddsFilePath = fileList.Find(o => o.EndsWith("wpn.dds"));
-            if (string.IsNullOrEmpty(objFilePath) || string.IsNullOrEmpty(ddsFilePath))
-            {
+            if (string.IsNullOrEmpty(objFilePath) || string.IsNullOrEmpty(ddsFilePath)) {
                 LoadingMask.Visibility = Visibility.Collapsed;
                 Utility.ShowException("MODEL ERROR");
                 return;
@@ -182,20 +153,19 @@ namespace RTDDE.Executer.Func
             ModelImporter importer = new ModelImporter();
             Model3DGroup objModel = importer.Load(objFilePath);
 
-            FIBITMAP fiBitmap = FreeImage.Load(FREE_IMAGE_FORMAT.FIF_DDS, ddsFilePath, FREE_IMAGE_LOAD_FLAGS.DEFAULT);
-            BitmapSource bitmapSource = BitmapToBitmapSource(FreeImage.GetBitmap(fiBitmap));
+            DDSImage image = await DDSReader.DDSReader.ReadImageAsync(ddsFilePath);
+            BitmapSource bitmapSource = await image.Frames.First().ToBitmapSource();
+
             ImageBrush textureBrush = new ImageBrush(bitmapSource) { ViewportUnits = BrushMappingMode.Absolute, TileMode = TileMode.Tile };
             var material = new DiffuseMaterial(textureBrush);
 
             List<GeometryModel3D> splitedModelList = new List<GeometryModel3D>();
-            if (objModel.Children.Count > 0)
-            {
+            if (objModel.Children.Count > 0) {
                 GeometryModel3D model3d = objModel.Children[0] as GeometryModel3D;
                 MeshGeometry3D mesh = model3d.Geometry as MeshGeometry3D;
                 //MeshGeometry3D sortedMesh = GetSortedMesh(mesh);
                 List<MeshGeometry3D> splitedMeshList = SplitMesh(mesh);
-                foreach (MeshGeometry3D splitedMesh in splitedMeshList)
-                {
+                foreach (MeshGeometry3D splitedMesh in splitedMeshList) {
                     GeometryModel3D splitModel = new GeometryModel3D();
                     splitModel.Geometry = splitedMesh;
                     splitModel.Material = material;
@@ -203,10 +173,10 @@ namespace RTDDE.Executer.Func
                     splitedModelList.Add(splitModel);
                 }
             }
-            if (objModel.Children.Count > 1 && string.IsNullOrEmpty(wpnddsFilePath) == false)
-            {
-                FIBITMAP fiBitmapWpn = FreeImage.Load(FREE_IMAGE_FORMAT.FIF_DDS, wpnddsFilePath, FREE_IMAGE_LOAD_FLAGS.DEFAULT);
-                BitmapSource bitmapSourceWpn = BitmapToBitmapSource(FreeImage.GetBitmap(fiBitmapWpn));
+            if (objModel.Children.Count > 1 && string.IsNullOrEmpty(wpnddsFilePath) == false) {
+                DDSImage imageWpn = await DDSReader.DDSReader.ReadImageAsync(wpnddsFilePath);
+                BitmapSource bitmapSourceWpn = await imageWpn.Frames.First().ToBitmapSource();
+
                 ImageBrush textureBrushWpn = new ImageBrush(bitmapSourceWpn) { ViewportUnits = BrushMappingMode.Absolute, TileMode = TileMode.Tile };
                 var materialWpn = new DiffuseMaterial(textureBrushWpn);
 
@@ -214,8 +184,7 @@ namespace RTDDE.Executer.Func
                 MeshGeometry3D mesh = model3d.Geometry as MeshGeometry3D;
                 //MeshGeometry3D sortedMesh = GetSortedMesh(mesh);
                 List<MeshGeometry3D> splitedMeshList = SplitMesh(mesh);
-                foreach (MeshGeometry3D splitedMesh in splitedMeshList)
-                {
+                foreach (MeshGeometry3D splitedMesh in splitedMeshList) {
                     GeometryModel3D splitModel = new GeometryModel3D();
                     splitModel.Geometry = splitedMesh;
                     splitModel.Material = materialWpn;
@@ -225,8 +194,7 @@ namespace RTDDE.Executer.Func
             }
             //group.Children = group.Children.OrderBy(o => (o as GeometryModel3D).Geometry.Bounds.Z);
             Model3DGroup group = new Model3DGroup();
-            foreach (GeometryModel3D splitedModel in splitedModelList.OrderBy(o => o.Geometry.Bounds.Z))
-            {
+            foreach (GeometryModel3D splitedModel in splitedModelList.OrderBy(o => o.Geometry.Bounds.Z)) {
                 group.Children.Add(splitedModel);
             }
             model.Content = group;
@@ -254,8 +222,7 @@ namespace RTDDE.Executer.Func
             Point3DCollection sortedPoints = new Point3DCollection();
             Dictionary<int, Point3D> pointDict = new Dictionary<int, Point3D>();
             int i = 0;
-            foreach (Point3D point in points)
-            {
+            foreach (Point3D point in points) {
                 pointDict.Add(i, point);
                 i++;
             }
@@ -263,8 +230,7 @@ namespace RTDDE.Executer.Func
 
             //get sort index & sorted Points
             i = 0;
-            foreach (KeyValuePair<int, Point3D> kv in pointDict)
-            {
+            foreach (KeyValuePair<int, Point3D> kv in pointDict) {
                 sortIndex.Add(i, kv.Key);
                 sortedPoints.Add(kv.Value);
                 i++;
@@ -273,16 +239,14 @@ namespace RTDDE.Executer.Func
             //use sort index to sort texture
             PointCollection texture = mesh.TextureCoordinates;
             PointCollection sortedTexture = new PointCollection();
-            foreach (KeyValuePair<int, int> kv in sortIndex)
-            {
+            foreach (KeyValuePair<int, int> kv in sortIndex) {
                 sortedTexture.Add(texture[kv.Value]);
             }
 
             //sort triangle
             Int32Collection triangle = mesh.TriangleIndices;
             Int32Collection sortedTriangle = new Int32Collection();
-            foreach (int tri in triangle)
-            {
+            foreach (int tri in triangle) {
                 sortedTriangle.Add(sortIndex.First(o => o.Value == tri).Key);
             }
 
@@ -296,10 +260,8 @@ namespace RTDDE.Executer.Func
         {
             Dictionary<double, List<int>> meshLayers = new Dictionary<double, List<int>>();
             int i = 0;
-            foreach (Point3D point in mesh.Positions)
-            {
-                if (meshLayers.Keys.Contains(point.Z) == false)
-                {
+            foreach (Point3D point in mesh.Positions) {
+                if (meshLayers.Keys.Contains(point.Z) == false) {
                     meshLayers.Add(point.Z, new List<int>());
                 }
                 meshLayers[point.Z].Add(i);
@@ -310,27 +272,23 @@ namespace RTDDE.Executer.Func
 
             List<MeshGeometry3D> splitedMeshList = new List<MeshGeometry3D>();
             int triCount = 0;
-            foreach (KeyValuePair<double, List<int>> kv in meshLayers)
-            {
+            foreach (KeyValuePair<double, List<int>> kv in meshLayers) {
                 int j = 0;
                 Dictionary<int, int> sortIndex = new Dictionary<int, int>();
                 Point3DCollection sortedPoints = new Point3DCollection();
                 PointCollection sortedTexture = new PointCollection();
                 Int32Collection sortedTriangle = new Int32Collection();
 
-                foreach (int index in kv.Value)
-                {
+                foreach (int index in kv.Value) {
                     sortIndex.Add(j, index);
                     sortedPoints.Add(mesh.Positions[index]);
                     sortedTexture.Add(mesh.TextureCoordinates[index]);
                     j++;
                 }
 
-                foreach (int tri in mesh.TriangleIndices)
-                {
+                foreach (int tri in mesh.TriangleIndices) {
                     //var splitTri = sortIndex.DefaultIfEmpty(new KeyValuePair<int, int>(-1, -1)).FirstOrDefault(o => o.Value == tri);
-                    if (sortIndex.Any(o => o.Value == tri))
-                    {
+                    if (sortIndex.Any(o => o.Value == tri)) {
                         sortedTriangle.Add(sortIndex.First(o => o.Value == tri).Key);
                     }
                 }
