e48df6b66d10d053a45c69df64493e6e20a35cf3
WindWT <leibao0723@gmail.com>
2015-09-30 21:35:39 +0800

    add trigger&action in enemy

--- RTDDE.Executer/Func/Enemy.xaml.cs
+++ RTDDE.Executer/Func/Enemy.xaml.cs
@@ -73,19 +73,18 @@ namespace RTDDE.Executer.Func
             EnemySearch_pattern.ItemsSource = patternDict;
         }
 
-        private void EnemyDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        private void EnemyDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             if (EnemyDataGrid.SelectedItem == null) {
                 //avoid Exception
                 return;
             }
-            string Enemyid = ((DataRowView)EnemyDataGrid.SelectedItem).Row["id"].ToString();
+            string Enemyid = ((DataRowView) EnemyDataGrid.SelectedItem).Row["id"].ToString();
             EnemyInfo_id.Text = Enemyid;
             EnemyInfo_lv.Text = "1";
             EnemyInfo_BindData(Enemyid);
         }
-        private void EnemyInfo_lv_LostFocus(object sender, RoutedEventArgs e)
-        {
+
+        private void EnemyInfo_lv_LostFocus(object sender, RoutedEventArgs e) {
             if (string.IsNullOrWhiteSpace(EnemyInfo_lv.Text)) {
                 EnemyInfo_lv.Text = "";
             }
@@ -97,70 +96,129 @@ namespace RTDDE.Executer.Func
             EnemyInfo_BindData(EnemyInfo_id.Text);
         }
 
-        public void EnemyInfo_BindData(string Enemyid)
-        {
+        public async void EnemyInfo_BindData(string Enemyid) {
             if (string.IsNullOrWhiteSpace(EnemyInfo_lv.Text)) {
                 return;
             }
             int thislevel = Convert.ToInt32(EnemyInfo_lv.Text);
 
-            Task<EnemyUnitMaster> task = new Task<EnemyUnitMaster>(() =>
-            {
+            Task<EnemyUnitMaster> taskEnemy = Task.Run(() => {
                 string sql = @"Select * from Enemy_unit_master WHERE id={0}";
                 return DAL.ToSingle<EnemyUnitMaster>(String.Format(sql, Enemyid));
             });
-            task.ContinueWith(t =>
-            {
-                if (t.Exception != null) {
-                    Utility.ShowException(t.Exception.InnerException.Message);
-                    return;
+            EnemyUnitMaster eum = await taskEnemy;
+
+            Task<LogicGroupData> taskLogic = Task.Run(() => {
+                string sql = @"Select * from LOGIC_GROUP_DATA WHERE logic_group_id={0}";
+                try {
+                    return DAL.ToSingle<LogicGroupData>(String.Format(sql, eum.logic_group_id));
                 }
-                if (task.Result == null) {
-                    return;
+                catch (Exception) {
+                    return null;
                 }
-                EnemyUnitMaster eum = task.Result;
-                EnemyInfo_id.Text = eum.id.ToString();
-                EnemyInfo_chara_flag_no.Text = eum.chara_flag_no == 0 ? string.Empty : eum.chara_flag_no.ToString();
-                int rare = eum.chara_symbol - 9;    //chara_symbol start from 10, equal to rare 1
-                string rareText = string.Empty;
-                if (rare >= 1 && rare <= 6) {
-                    for (int i = 0; i < rare; i++) {
-                        rareText += "★";
+            });
+            
+            EnemyInfo_id.Text = eum.id.ToString();
+            EnemyInfo_chara_flag_no.Text = eum.chara_flag_no == 0 ? string.Empty : eum.chara_flag_no.ToString();
+            int rare = eum.chara_symbol - 9; //chara_symbol start from 10, equal to rare 1
+            string rareText = string.Empty;
+            if (rare >= 1 && rare <= 6) {
+                for (int i = 0; i < rare; i++) {
+                    rareText += "★";
+                }
+            }
+            EnemyInfo_chara_symbol.Text = rareText;
+            EnemyInfo_name.Text = eum.name;
+            EnemyInfo_attribute.Text = Utility.ParseAttributetype(eum.attribute);
+            EnemyInfo_type.Text = Utility.ParseEnemyType(eum.type);
+            EnemyInfo_chara_kind.Text = eum.chara_flag_no == 0
+                ? string.Empty
+                : Utility.ParseUnitKind(eum.chara_kind).ToString();
+
+            EnemyInfo_lv.Text = thislevel.ToString("0");
+            EnemyInfo_HP.Text = Utility.RealCalc(eum.life, eum.up_life, thislevel).ToString();
+            EnemyInfo_ATK.Text = Utility.RealCalc(eum.attack, eum.up_attack, thislevel).ToString();
+            EnemyInfo_DEF.Text = Utility.RealCalc(eum.defense, eum.up_defense, thislevel).ToString();
+            EnemyInfo_soul_pt.Text = eum.soul_pt.ToString();
+            EnemyInfo_gold_pt.Text = eum.gold_pt.ToString();
+            EnemyInfo_flag.Text = Convert.ToBoolean(eum.flag).ToString();
+            EnemyInfo_isUnit.Text = Utility.IsUnitEnemy(eum.type).ToString();
+            EnemyInfo_turn.Text = eum.turn.ToString();
+            EnemyInfo_ui.Text = eum.ui.ToString();
+            //skill
+            EnemyInfo_pat.Text = Utility.ParseAttackPattern(eum.pat);
+            EnemyInfo_p0.Text = eum.p0.ToString();
+            EnemyInfo_p1.Text = eum.p1.ToString();
+            EnemyInfo_pat_01.Text = Utility.ParseAttackPattern(eum.pat_01);
+            EnemyInfo_p0_01.Text = eum.p0_01.ToString();
+            EnemyInfo_p1_01.Text = eum.p1_01.ToString();
+            //Logic
+            EnemyLogicStackPanel.Children.Clear();
+            if (eum.logic_group_id == 0) {
+                EnemyLogicExpander.Visibility = Visibility.Collapsed;
+                EnemyLogicExpander.Header = "Logic";
+            }
+            else {
+                EnemyLogicExpander.Visibility=Visibility.Visible;
+                EnemyLogicExpander.Header = $"Logic#{eum.logic_group_id}";
+                LogicGroupData lgd = await taskLogic;
+                if (lgd != null) {
+                    foreach (LogicData logicData in lgd.logic_data) {
+                        Grid logicGrid=new Grid();
+                        logicGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(50) });
+                        logicGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                        logicGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(50) });
+                        logicGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(50) });
+                        logicGrid.RowDefinitions.Add(new RowDefinition() {Height = new GridLength(1, GridUnitType.Star)});
+                        
+                        var idTextbox = new TextBox() { Text = logicData.logic_id.ToString() };
+                        idTextbox.SetValue(Grid.ColumnProperty, 0);
+                        idTextbox.SetValue(Grid.RowProperty, 0);
+                        logicGrid.Children.Add(idTextbox);
+
+                        var typeTextbox = new TextBox() { Text = Utility.ParseTriggerType(logicData.trigger_type) };
+                        typeTextbox.SetValue(Grid.ColumnProperty, 1);
+                        typeTextbox.SetValue(Grid.ColumnSpanProperty, 2);
+                        typeTextbox.SetValue(Grid.RowProperty, 0);
+                        logicGrid.Children.Add(typeTextbox);
+
+                        var paramTextbox = new TextBox() { Text = logicData.trigger_param.ToString() };
+                        paramTextbox.SetValue(Grid.ColumnProperty, 3);
+                        paramTextbox.SetValue(Grid.RowProperty, 0);
+                        logicGrid.Children.Add(paramTextbox);
+
+                        int actionCount = 1; //use for rowProperty
+                        foreach (ActionData actionData in logicData.action_data) {
+                            logicGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+
+                            var actionTypeTextbox = new TextBox() { Text = Utility.ParseAttackPattern(actionData.action_type) };
+                            actionTypeTextbox.SetValue(Grid.ColumnProperty, 1);
+                            actionTypeTextbox.SetValue(Grid.RowProperty, actionCount);
+                            logicGrid.Children.Add(actionTypeTextbox);
+
+                            var param0Textbox = new TextBox() { Text = actionData.action_param_0.ToString() };
+                            param0Textbox.SetValue(Grid.ColumnProperty, 2);
+                            param0Textbox.SetValue(Grid.RowProperty, actionCount);
+                            logicGrid.Children.Add(param0Textbox);
+
+                            var param1Textbox = new TextBox() { Text = actionData.action_param_1.ToString() };
+                            param1Textbox.SetValue(Grid.ColumnProperty, 3);
+                            param1Textbox.SetValue(Grid.RowProperty, actionCount);
+                            logicGrid.Children.Add(param1Textbox);
+
+                            actionCount++;
+                        }
+                        EnemyLogicStackPanel.Children.Add(logicGrid);
                     }
                 }
-                EnemyInfo_chara_symbol.Text = rareText;
-                EnemyInfo_name.Text = eum.name;
-                EnemyInfo_attribute.Text = Utility.ParseAttributetype(eum.attribute);
-                EnemyInfo_type.Text = Utility.ParseEnemyType(eum.type);
-                EnemyInfo_chara_kind.Text = eum.chara_flag_no == 0 ? string.Empty : Utility.ParseUnitKind(eum.chara_kind).ToString();
-
-                EnemyInfo_lv.Text = thislevel.ToString("0");
-                EnemyInfo_HP.Text = Utility.RealCalc(eum.life, eum.up_life, thislevel).ToString();
-                EnemyInfo_ATK.Text = Utility.RealCalc(eum.attack, eum.up_attack, thislevel).ToString();
-                EnemyInfo_DEF.Text = Utility.RealCalc(eum.defense, eum.up_defense, thislevel).ToString();
-                EnemyInfo_soul_pt.Text = eum.soul_pt.ToString();
-                EnemyInfo_gold_pt.Text = eum.gold_pt.ToString();
-                EnemyInfo_flag.Text = Convert.ToBoolean(eum.flag).ToString();
-                EnemyInfo_isUnit.Text = Utility.IsUnitEnemy(eum.type).ToString();
-                EnemyInfo_turn.Text = eum.turn.ToString();
-                EnemyInfo_ui.Text = eum.ui.ToString();
-
-                EnemyInfo_pat.Text = Utility.ParseAttackPattern(eum.pat);
-                EnemyInfo_p0.Text = eum.p0.ToString();
-                EnemyInfo_p1.Text = eum.p1.ToString();
-                EnemyInfo_pat_01.Text = Utility.ParseAttackPattern(eum.pat_01);
-                EnemyInfo_p0_01.Text = eum.p0_01.ToString();
-                EnemyInfo_p1_01.Text = eum.p1_01.ToString();
-                //Advanced
-                EnemyInfo_model.Text = eum.model;
-                EnemyInfo_texture.Text = eum.texture;
-                EnemyInfo_icon.Text = eum.icon.ToString();
-                EnemyInfo_shadow.Text = eum.shadow.ToString();
-                EnemyInfo_up.Text = eum.up.ToString();
-                EnemyInfo_atk_ef_id.Text = eum.atk_ef_id.ToString();
-                EnemyInfo_logic_group_id.Text = eum.logic_group_id.ToString();
-            }, MainWindow.UiTaskScheduler);    //this Task work on ui thread
-            task.Start();
+            }
+            //Advanced
+            EnemyInfo_model.Text = eum.model;
+            EnemyInfo_texture.Text = eum.texture;
+            EnemyInfo_icon.Text = eum.icon.ToString();
+            EnemyInfo_shadow.Text = eum.shadow.ToString();
+            EnemyInfo_up.Text = eum.up.ToString();
+            EnemyInfo_atk_ef_id.Text = eum.atk_ef_id.ToString();
         }
 
         private void EnemySearchClear_Click(object sender, RoutedEventArgs e)
