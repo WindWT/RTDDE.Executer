76ff9bb3393269c86bcc5791e4f673353900b3d1
WindWT <leibao0723@gmail.com>
2016-07-30 11:07:42 +0800

    update year

--- RTDDE.Executer/Properties/AssemblyInfo.cs
+++ RTDDE.Executer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RTDDE.Executer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
